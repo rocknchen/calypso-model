ae46eca2db50279d2d72e8409380392b5d544d5c
rockchen <rochchen@htsc.com>
2024-01-02 16:16:35 +0800

    update

---
+++ src/main/java/com/hthk/calypsox/model/trade/criteria/CriteriaGetTrade.java
@@ -0,0 +1,25 @@
+package com.hthk.calypsox.model.trade.criteria;
+
+import com.hthk.fintech.model.common.Criteria;
+import com.hthk.fintech.model.software.app.ApplicationEnum;
+
+import static com.hthk.fintech.model.data.datacenter.query.EntityTypeEnum.TRADE;
+import static com.hthk.fintech.model.web.http.ActionTypeEnum.GET;
+
+/**
+ * @Author: Rock CHEN
+ * @Date: 2024/1/2 15:58
+ */
+@Criteria(action = GET, type = TRADE, appName = ApplicationEnum.CALYPSO)
+public class CriteriaGetTrade extends com.hthk.fintech.model.finance.trade.criteria.CriteriaGetTrade {
+
+    private String tradeFilter;
+
+    public String getTradeFilter() {
+        return tradeFilter;
+    }
+
+    public void setTradeFilter(String tradeFilter) {
+        this.tradeFilter = tradeFilter;
+    }
+}
