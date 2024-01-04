169815b8eb2b0af93ee8eaae1d6e192cea91b8d2
rockchen <rochchen@htsc.com>
2024-01-04 10:25:04 +0800

    update

--- src/main/java/com/hthk/calypsox/model/trade/TradeInfo.java
+++ src/main/java/com/hthk/calypsox/model/trade/TradeInfo.java
@@ -5,4 +5,14 @@ package com.hthk.calypsox.model.trade;
  * @Date: 2024/1/2 17:37
  */
 public class TradeInfo {
+
+    private String trader;
+
+    public String getTrader() {
+        return trader;
+    }
+
+    public void setTrader(String trader) {
+        this.trader = trader;
+    }
 }
