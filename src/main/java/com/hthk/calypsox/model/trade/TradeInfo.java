9ee814b2d061fb7a6ca99a088301d1b41399eaf5
rockchen <rochchen@htsc.com>
2024-01-05 09:10:10 +0800

    update

--- src/main/java/com/hthk/calypsox/model/trade/TradeInfo.java
+++ src/main/java/com/hthk/calypsox/model/trade/TradeInfo.java
@@ -1,10 +1,16 @@
 package com.hthk.calypsox.model.trade;
 
+import java.time.LocalDateTime;
+
 /**
  * @Author: Rock CHEN
  * @Date: 2024/1/2 17:37
  */
 public class TradeInfo {
+
+    private String book;
+
+    private LocalDateTime tradeDateTime;
 
     private String trader;
 
@@ -14,5 +20,21 @@ public class TradeInfo {
 
     public void setTrader(String trader) {
         this.trader = trader;
+    }
+
+    public String getBook() {
+        return book;
+    }
+
+    public void setBook(String book) {
+        this.book = book;
+    }
+
+    public LocalDateTime getTradeDateTime() {
+        return tradeDateTime;
+    }
+
+    public void setTradeDateTime(LocalDateTime tradeDateTime) {
+        this.tradeDateTime = tradeDateTime;
     }
 }
