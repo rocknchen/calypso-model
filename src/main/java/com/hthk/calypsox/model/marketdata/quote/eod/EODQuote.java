ca1db65ba9e2ba0342a01d2e8b4f430f51633676
rockchen <rochchen@htsc.com>
2024-01-15 17:53:05 +0800

    update

--- src/main/java/com/hthk/calypsox/model/marketdata/quote/eod/EODQuote.java
+++ src/main/java/com/hthk/calypsox/model/marketdata/quote/eod/EODQuote.java
@@ -2,6 +2,7 @@ package com.hthk.calypsox.model.marketdata.quote.eod;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 /**
  * @Author: Rock CHEN
@@ -9,11 +10,121 @@ import java.time.LocalDate;
  */
 public class EODQuote {
 
-    private String quoteName;
-
     private LocalDate date;
 
+    private String quoteName;
+
+    private String quoteType;
+
+    private BigDecimal bid;
+
+    private BigDecimal ask;
+
+    private BigDecimal open;
+
     private BigDecimal close;
+
+    private BigDecimal high;
+
+    private BigDecimal low;
+
+    private BigDecimal last;
+
+    private LocalDateTime enteredDate;
+
+    private String enteredUser;
+
+    private LocalDate knownDate;
+
+    private String sourceName;
+
+    public String getQuoteType() {
+        return quoteType;
+    }
+
+    public void setQuoteType(String quoteType) {
+        this.quoteType = quoteType;
+    }
+
+    public BigDecimal getBid() {
+        return bid;
+    }
+
+    public void setBid(BigDecimal bid) {
+        this.bid = bid;
+    }
+
+    public BigDecimal getAsk() {
+        return ask;
+    }
+
+    public void setAsk(BigDecimal ask) {
+        this.ask = ask;
+    }
+
+    public BigDecimal getOpen() {
+        return open;
+    }
+
+    public void setOpen(BigDecimal open) {
+        this.open = open;
+    }
+
+    public BigDecimal getHigh() {
+        return high;
+    }
+
+    public void setHigh(BigDecimal high) {
+        this.high = high;
+    }
+
+    public BigDecimal getLow() {
+        return low;
+    }
+
+    public void setLow(BigDecimal low) {
+        this.low = low;
+    }
+
+    public BigDecimal getLast() {
+        return last;
+    }
+
+    public void setLast(BigDecimal last) {
+        this.last = last;
+    }
+
+    public LocalDateTime getEnteredDate() {
+        return enteredDate;
+    }
+
+    public void setEnteredDate(LocalDateTime enteredDate) {
+        this.enteredDate = enteredDate;
+    }
+
+    public String getEnteredUser() {
+        return enteredUser;
+    }
+
+    public void setEnteredUser(String enteredUser) {
+        this.enteredUser = enteredUser;
+    }
+
+    public LocalDate getKnownDate() {
+        return knownDate;
+    }
+
+    public void setKnownDate(LocalDate knownDate) {
+        this.knownDate = knownDate;
+    }
+
+    public String getSourceName() {
+        return sourceName;
+    }
+
+    public void setSourceName(String sourceName) {
+        this.sourceName = sourceName;
+    }
 
     public LocalDate getDate() {
         return date;
