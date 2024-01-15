f89b83083a394c2227e3d46137a0c1b12a99728c
rockchen <rochchen@htsc.com>
2024-01-15 18:47:50 +0800

    update

--- src/main/java/com/hthk/calypsox/model/quote/CriteriaEODQuote.java
+++ src/main/java/com/hthk/calypsox/model/quote/CriteriaEODQuote.java
@@ -4,14 +4,13 @@ import com.hthk.fintech.model.common.Criteria;
 import com.hthk.fintech.model.software.app.ApplicationEnum;
 
 import static com.hthk.fintech.model.data.datacenter.query.EntityTypeEnum.EOD_QUOTE;
-import static com.hthk.fintech.model.web.http.ActionTypeEnum.COUNT;
-import static com.hthk.fintech.model.web.http.ActionTypeEnum.GET;
+import static com.hthk.fintech.model.web.http.ActionTypeEnum.*;
 
 /**
  * @Author: Rock CHEN
  * @Date: 2024/1/5 14:29
  */
-@Criteria(actions = {GET, COUNT}, type = EOD_QUOTE, appName = ApplicationEnum.CALYPSO)
+@Criteria(actions = {GET, COUNT, SYNCHRONIZE}, type = EOD_QUOTE, appName = ApplicationEnum.CALYPSO)
 public class CriteriaEODQuote extends com.hthk.fintech.model.finance.quote.CriteriaEODQuote {
 
     private String quoteName;
