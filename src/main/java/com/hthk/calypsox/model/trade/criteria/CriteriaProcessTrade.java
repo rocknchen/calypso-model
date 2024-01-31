7e5412d6a0461263874e2c1da58192ccd5342a4e
rockchen <rochchen@htsc.com>
2024-01-31 15:47:12 +0800

    update

--- src/main/java/com/hthk/calypsox/model/trade/criteria/CriteriaProcessTrade.java
+++ src/main/java/com/hthk/calypsox/model/trade/criteria/CriteriaProcessTrade.java
@@ -3,6 +3,8 @@ package com.hthk.calypsox.model.trade.criteria;
 import com.hthk.fintech.model.common.Criteria;
 import com.hthk.fintech.model.software.app.ApplicationEnum;
 
+import java.util.List;
+
 import static com.hthk.fintech.model.data.datacenter.query.EntityTypeEnum.TRADE;
 import static com.hthk.fintech.model.web.http.ActionTypeEnum.*;
 
@@ -15,7 +17,17 @@ public class CriteriaProcessTrade {
 
     private String tradeFilter;
 
+    private List<String> bookList;
+
     private String applyAction;
+
+    public List<String> getBookList() {
+        return bookList;
+    }
+
+    public void setBookList(List<String> bookList) {
+        this.bookList = bookList;
+    }
 
     public String getTradeFilter() {
         return tradeFilter;
