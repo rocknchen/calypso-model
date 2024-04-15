9cd8f96b446ff326163b2fc5ce4780241ba5ad4c
rockchen <rochchen@htsc.com>
2024-04-15 16:58:59 +0800

    update

--- src/main/java/com/hthk/calypsox/model/person/UserInfo.java
+++ src/main/java/com/hthk/calypsox/model/person/UserInfo.java
@@ -16,6 +16,26 @@ public class UserInfo {
 
     private List<String> groupList;
 
+    private List<String> bookReadWrite;
+
+    private List<String> bookReadOnly;
+
+    public List<String> getBookReadWrite() {
+        return bookReadWrite;
+    }
+
+    public void setBookReadWrite(List<String> bookReadWrite) {
+        this.bookReadWrite = bookReadWrite;
+    }
+
+    public List<String> getBookReadOnly() {
+        return bookReadOnly;
+    }
+
+    public void setBookReadOnly(List<String> bookReadOnly) {
+        this.bookReadOnly = bookReadOnly;
+    }
+
     public String getUserName() {
         return userName;
     }
